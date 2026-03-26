d58e0d85026942bde764bea8561fd169689c9c70
osmelyyy <osmely.german-ortega27@compscihigh.org>
2026-03-26 16:46:46 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,38 @@
 import "./App.css";
 
 // 3. import useState from react
-
+import { useState } from 'react'
 
 function App() {
   // 4. Create useState variables for mood called mood and setMood, make the default "Tired"
-
+  const [mood, setMood] = useState("Tired");
 
 
   // 5. Create useState variables for energy called energy and setEnergy, make the default 3
 
-
+  const [energy, setEnergy] = useState(3);
 
   // 6. Create useState variables for school mode called schoolMode and setSchoolMode, make the default true
-
+  const [schoolMode, setSchoolMode] = useState(true)
 
   // 7. Create a function called handleHappy
   // When the function runs:
   // - change mood to "Happy"
   // - show an alert about your mood improving
-
+  function handleHappy() {
+    setMood('Happy');
+    alert("Mood upgraded.");
+  }
 
 
   // 8. Create a function called handleEnergyUp
   // When the function runs:
   // - increase energy by 1
   // - show an alert about your energy rising
-
+  function handleEnergyUp() {
+    setEnergy + 1
+    alert("Energy rising")
+  }
 
 
   // 9. Create a function called handleReset
@@ -35,14 +41,21 @@ function App() {
   // - reset energy back to 3
   // - reset schoolMode back to true
   // - show an alert that everything has been reset
-
+function handleReset() {
+  mood = "Tired";
+  energy = 3;
+  schoolMode = true;
+  alert("Everything has been reset");
+}
 
 
   // 10. Create a function called handleSchoolMode
   // When the function runs:
   // - switch schoolMode between true and false
-
-
+function handleSchoolMode(){
+  schoolMode = schoolMode
+}
+ 
 
   return (
     <div className="page">
@@ -59,13 +72,14 @@ function App() {
 
             <button
               className="btn"
-              // 11. Connect this button to handleHappy using onClick
+            // 11. Connect this button to handleHappy using onClick
+            onClick={handleHappy}
             >
               Change Mood to Happy 🙂
             </button>
 
             {/* 12. Display mood here using curly brackets */}
-            <p className="output">Mood: </p>
+            <p className="output">Mood: {} </p>
           </div>
 
           <div className="tracker-box">
@@ -74,32 +88,35 @@ function App() {
 
             <button
               className="btn"
-              // 13. Connect this button to handleEnergyUp using onClick
+            // 13. Connect this button to handleEnergyUp using onClick
+            onClick={handleEnergyUp}
             >
               Increase Energy ⚡
             </button>
 
             {/* 14. Display energy here using curly brackets */}
-            <p className="output">Energy: </p>
+            <p className="output">Energy: {} </p>
           </div>
         </div>
 
         <button
           className="btn secondary"
-          // 15. Connect this button to handleReset using onClick
+        // 15. Connect this button to handleReset using onClick
+        onClick={handleReset}
         >
           Reset Tracker
         </button>
 
         <button
           className="btn secondary"
-          // 16. Connect this button to handleSchoolMode using onClick
+        // 16. Connect this button to handleSchoolMode using onClick
+        onClick={handleSchoolMode}
         >
           Toggle School Mode
         </button>
 
         {/* 17. Add this after the colon: schoolMode ? "School Mode: ON" : "School Mode: OFF" */}
-        <p className="mode">Current Mode: </p>
+        <p className="mode">Current Mode: {schoolMode ? "School Mode: ON" : "School Mode: OFF"}</p>
 
         {/* 18. Submit! (git add . git commit -m 'done' git push) */}
       </section>
